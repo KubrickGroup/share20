fce426af275e5c20f6fc50d29d8bdb1af64959bd
David Portas <davidportas@kubrickgroup.com>
2022-05-05 09:02:56 +0000

    Update asbrand_sale_ddl.sql

--- asbrand_sale_ddl.sql
+++ asbrand_sale_ddl.sql
@@ -8,6 +8,10 @@ CREATE TABLE XXXXX.sale
 	qty int NOT NULL,
 	value decimal(10, 2) NULL,
 	color VARCHAR(50) NULL,
-	brand_name nvarchar(50) NULL
+	brand_name nvarchar(50) NULL,
+	login_name varchar(20) NOT NULL,
+	full_name varchar(100) NOT NULL,
+	city varchar(50) NOT NULL,
+	postal_code varchar(10) NOT NULL,
+	email nvarchar(255) NOT NULL
 );
-
