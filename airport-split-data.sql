005ee835e0bd32b23ecd9e6ad8fa3f549fc1cc85
David Portas <davidportas@kubrickgroup.com>
2022-05-30 21:44:34 +0000

    Upload New File

---
+++ airport-split-data.sql
@@ -0,0 +1,24 @@
+-- Airport conditional split tables. Replace XXXXXXs with a schema name!
+
+CREATE TABLE XXXXXX.closedairport (
+	ident varchar(10) NOT NULL PRIMARY KEY,
+	iso_country char(2) NOT NULL,
+	name nvarchar(100) NOT NULL,
+	type varchar(50) NOT NULL
+)
+
+CREATE TABLE XXXXXX.activeairport (
+	ident varchar(10) NOT NULL PRIMARY KEY,
+	iso_country char(2) NOT NULL,
+	name nvarchar(100) NOT NULL,
+	type varchar(50) NOT NULL
+)
+
+CREATE TABLE XXXXXX.iataairport (
+	ident varchar(10) NOT NULL PRIMARY KEY,
+	iso_country char(2) NOT NULL,
+	name nvarchar(100) NOT NULL,
+	type varchar(50) NOT NULL
+	iata_code varchar(10) NOT NULL
+);
+
