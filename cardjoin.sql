230327c58b966028fb78e860b5f21e40f518960e
David Portas <davidportas@kubrickgroup.com>
2022-05-31 07:46:39 +0000

    Upload New File

---
+++ cardjoin.sql
@@ -0,0 +1,21 @@
+CREATE TABLE XXXXXX.cardjoin (
+	 clientnum bigint NOT NULL,
+	 customer_age int NULL,
+	 card_category varchar (100) NULL,
+	 trans_id INTEGER NOT NULL,
+	 amount decimal (10, 2) NOT NULL,
+	 trans_date date NOT NULL,
+	 balance decimal (10, 2) NOT NULL,
+	 CONSTRAINT pk_cardjoin PRIMARY KEY (clientnum,transid)
+);
+
+CREATE TABLE XXXXXX.cardlatest (
+	 clientnum bigint NOT NULL,
+	 customer_age int NULL,
+	 card_category varchar (100) NULL,
+	 trans_id INTEGER NOT NULL,
+	 amount decimal (10, 2) NOT NULL,
+	 trans_date date NOT NULL,
+	 balance decimal (10, 2) NOT NULL,
+	 CONSTRAINT pk_cardlatest PRIMARY KEY (clientnum)
+);
