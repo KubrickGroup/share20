b0bb004fb001bf024ceb34161f888f529578ab91
David Portas <davidportas@kubrickgroup.com>
2022-05-31 07:59:51 +0000

    Update cardjoin.sql

--- cardjoin.sql
+++ cardjoin.sql
@@ -2,20 +2,18 @@ CREATE TABLE XXXXXX.cardjoin (
 	 clientnum bigint NOT NULL,
 	 customer_age int NULL,
 	 card_category varchar (100) NULL,
-	 trans_id INTEGER NOT NULL,
-	 amount decimal (10, 2) NOT NULL,
-	 trans_date date NOT NULL,
-	 balance decimal (10, 2) NOT NULL,
-	 CONSTRAINT pk_cardjoin PRIMARY KEY (clientnum,trans_id)
+	 trans_id INTEGER  NULL,
+	 amount decimal (10, 2) NULL,
+	 trans_date date NULL,
+	 balance decimal (10, 2) NULL
 );
 
 CREATE TABLE XXXXXX.cardlatest (
 	 clientnum bigint NOT NULL,
 	 customer_age int NULL,
 	 card_category varchar (100) NULL,
-	 trans_id INTEGER NOT NULL,
-	 amount decimal (10, 2) NOT NULL,
-	 trans_date date NOT NULL,
-	 balance decimal (10, 2) NOT NULL,
-	 CONSTRAINT pk_cardlatest PRIMARY KEY (clientnum)
+	 trans_id INTEGER NULL,
+	 amount decimal (10, 2) NULL,
+	 trans_date date NULL,
+	 balance decimal (10, 2) NULL
 );
